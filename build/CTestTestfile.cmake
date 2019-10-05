91401ff6546a351ef48a240ea0354e97c95b0f7a
AdityaGawali <adityagawali13@gmail.com>
2019-10-05 16:56:22 +0530

    build devel remove

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aditya/manipulator_ws/src
-# Build directory: /home/aditya/manipulator_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(mobile_manipulator)
--- build/mobile_manipulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aditya/manipulator_ws/src/mobile_manipulator
-# Build directory: /home/aditya/manipulator_ws/build/mobile_manipulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
