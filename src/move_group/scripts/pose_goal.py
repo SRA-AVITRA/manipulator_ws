aea2ac5ef19af85378939636599c200fcdd08aa9
AdityaGawali <adityagawali13@gmail.com>
2019-10-10 12:01:44 +0530

    pose_goal refine

--- src/move_group/scripts/pose_goal.py
+++ src/move_group/scripts/pose_goal.py
@@ -8,6 +8,7 @@ import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
+import tf
 
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('pose_goal',anonymous=True)
@@ -21,35 +22,43 @@ group = moveit_commander.MoveGroupCommander(group_name)
 
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory,queue_size=20)
 
-# We can get the name of the reference frame for this robot:
-planning_frame = group.get_planning_frame()
-print "============ Reference frame: %s" % planning_frame
 
-# We can also print the name of the end-effector link for this group:
-eef_link = group.get_end_effector_link()
-print "============ End effector: %s" % eef_link
 
-# We can get a list of all the groups in the robot:
-group_names = robot.get_group_names()
-print "============ Robot Groups:", robot.get_group_names()
+roll = 1.57
+pitch = 1.57
+yaw = 1.57
 
-# Sometimes for debugging it is useful to print the entire state of the
-# robot:
-print "============ Printing robot state"
-print robot.get_current_state()
-print ""
+quat = tf.transformations.quaternion_from_euler(roll,pitch,yaw)
+print quat
+if (len(sys.argv) == 2):
+    if(sys.argv[1]== "home"): 
+        joint_goal =group.get_current_joint_values()
+        joint_goal[0] = 0
+        joint_goal[1] = 0
+        joint_goal[2] = 0
+        joint_goal[3] = 0
+        joint_goal[4] = 0
+        joint_goal[5] = 0
+        
+        # The go command can be called with joint values, poses, or without any
+        # parameters if you have already set the pose or joint target for the group
+        group.go(joint_goal, wait=True)
+
+        # Calling ``stop()`` ensures that there is no residual movement
+        group.stop()
+
 
 
 
 pose_goal = geometry_msgs.msg.Pose()
-pose_goal.orientation.w = 0.35
-pose_goal.orientation.x = 0.35
-pose_goal.orientation.y = 0.35
-pose_goal.orientation.z = 0.35
+pose_goal.orientation.w = quat[3]
+pose_goal.orientation.x = quat[0]
+pose_goal.orientation.y = quat[1]
+pose_goal.orientation.z = quat[2]
 
-pose_goal.position.x = 0.3
-pose_goal.position.y = 0.3
-pose_goal.position.z = 0.3
+pose_goal.position.x = 0
+pose_goal.position.y = 0
+pose_goal.position.z = 0
 group.set_pose_target(pose_goal)
 
 plan = group.go(wait=True)
@@ -58,3 +67,5 @@ group.stop()
 # It is always good to clear your targets after planning with poses.
 # Note: there is no equivalent function for clear_joint_value_targets()
 group.clear_pose_targets()
+print "ROBOT STATE :"
+print robot.get_current_state()
