a6618173cafd03ad2b24e390bd35627942e5be05
AdityaGawali <adityagawali13@gmail.com>
2019-11-02 11:54:12 +0530

    change limits in xacro

--- src/move_group/scripts/cartesian.py
+++ src/move_group/scripts/cartesian.py
@@ -20,12 +20,10 @@ move_group = moveit_commander.MoveGroupCommander(group_name)
 
 display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory,queue_size=20)
 
-start = [0.454690388429,0.000719827773983,0.476488014448,0.00349018704767,0.712495632033,0.000152936112879,0.701667848444]
-
+pre_grasp = [0.454690388429,0.000719827773983,0.476488014448,0.00349018704767,0.712495632033,0.000152936112879,0.701667848444]
 home = [0.195131774248,0.00191430998864, 0.777714415449,9.14285230913e-05,-0.00179983146408,0.00281640408846,0.999994410042]
-
-
-
+table =  [0.446840677509,-0.0300459494516,0.5902446131,0.0404753932932,0.695521690797,-0.0279992592663,0.716817523257]
+board = [0.380466626296,-0.109957891144,0.43512853529, 0.039533297085, 0.717726803196,0.0392843173892,0.694090841896]
 
 
 def change_end_pose(group, input_pose):
@@ -55,7 +53,7 @@ def change_joint_angles(group, joint_goal):
 def init(group):
     change_end_pose(group, home)
     print("HOME")
-    change_end_pose(group, start)
+    change_end_pose(group, board)
     print("START")
     
 scale = 0.01
@@ -78,10 +76,10 @@ def draw(x,y,z):
 init(move_group)
 #change_joint_angles(move_group,[0,0,0,0,0,0])
 
-draw(0,5,0)
-draw(5,0,0)
-draw(0,-5,0)
-draw(-5,0,0)
+draw(0,10,0)
+draw(10,0,0)
+draw(0,-10,0)
+draw(-10,0,0)
 
 
 
---
+++ src/perception/scripts/get_objectarray_realsense.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import rospy 
+import sys
+import cv2
+from perception.msg import array
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np 
+
+
+node_name = "cv_bridge_demo"
+
+#Initialize the ros node
+rospy.init_node(node_name)
+# What we do during shutdown
+
+
+rospy.loginfo("Waiting for image topics...")
+
+def image_callback(ros_image):
+	bridge = CvBridge()
+	kernel = np.ones((2,2),np.uint8)
+	lower_red = np.array([ 110.,   100.,  100.])
+	upper_red = np.array([ 120.,  220.,  210.])
+
+	try:
+		frame = bridge.imgmsg_to_cv2(ros_image, "bgr8")
+	except CvBridgeError, e:
+		print e
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	mask_red = cv2.inRange(hsv, lower_red, upper_red)
+	mask_red = cv2.bilateralFilter(mask_red,9,75,75)
+	mask_red = cv2.dilate(mask_red,kernel,iterations=1)
+
+	cv2.imshow("red",mask_red)
+	_, contoursRed, hR = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	
+	idx,current_max,counter = 0, 0, 0	
+	for n in contoursRed:
+		a = cv2.contourArea(n)
+		if a>current_max:
+			current_max=a
+			idx = counter
+		counter+=1
+
+
+	redIndex, redValue = idx, current_max
+    cimg = np.zeros_like(img)
+    cv2.drawContours(cimg, contoursRed, redIndex, color=255, thickness=-1)
+    pts = np.where(cimg == 255)
+    print(pts)
+
+	centroid_pub.publish(pts)
+	cv2.drawContours(frame, contoursRed, redIndex, (0, 0, 255), 2)
+	cv2.imshow(node_name, frame)
+	keystroke = cv2.waitKey(5)
+	if keystroke  == 27:
+		rospy.shutdown()
+		cv2.DestroyAllWindows()
+
+
+def main(args):
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down vision node."
+		cv2.DestroyAllWindows()
+
+if __name__ == '__main__':
+	image_sub = rospy.Subscriber("/camera/rgb/image_color", Image,image_callback)
+	centroid_pub = rospy.Publisher('centroid', array, queue_size = 10)
+
+	main(sys.argv)
