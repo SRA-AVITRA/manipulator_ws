ee4c9fb4d8b98811d584d2022335b659d513c810
shambhavi19 <kutheshambhavi1@gmail.com>
2019-11-06 12:43:05 +0530

    home.launch added to execute.launch

---
+++ src/move_group/scripts/home.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list, list_to_pose
+import tf
+
+def change_joint_angles(group, joint_goal):
+    joints = group.get_current_joint_values()
+    for i in range(len(joint_goal)):
+        joints[i] = joint_goal[i]
+    group.go(joints, wait=True)
+    group.stop()
+    print "\nJoint angles moved to ", joints
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('home', anonymous=True)
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group_arm = moveit_commander.MoveGroupCommander("arm")
+group_arm.set_planner_id("TRRT")
+
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory, queue_size=20)
+
+planning_frame = group_arm.get_planning_frame()
+group_arm.set_goal_tolerance(0.0001)
+group_arm.set_goal_orientation_tolerance(0.0001)
+
+eef_link = group_arm.get_end_effector_link()
+group_names = robot.get_group_names()
+
+change_joint_angles(group_arm, [0, 0, 0, 0, 0, 0])
+
