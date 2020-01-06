d02378af91de80f15a364a8c68b95931175f6470
saa1605 <saaket.agashe@gmail.com>
2020-01-06 14:50:04 +0530

    added robond, new segmentation file

---
+++ src/perception/scripts/pcl_segmentation.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import rospy
+import pcl
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs.point_cloud2 as pc2
+import ros_numpy
+
+
+def ros_to_pcl(ros_cloud):
+    """ Converts a ROS PointCloud2 message to a pcl PointXYZRGB
+
+        Args:
+            ros_cloud (PointCloud2): ROS PointCloud2 message
+
+        Returns:
+            pcl.PointCloud_PointXYZRGB: PCL XYZRGB point cloud
+    """
+    points_list = []
+
+    for data in pc2.read_points(ros_cloud, skip_nans=True):
+        points_list.append([data[0], data[1], data[2], data[3]])
+
+    pcl_data = pcl.PointCloud_PointXYZRGB()
+    pcl_data.from_list(points_list)
+
+    return pcl_data
+
+def pcl_to_ros(pcl_array):
+    """ Converts a pcl PointXYZRGB to a ROS PointCloud2 message
+
+        Args:
+            pcl_array (PointCloud_PointXYZRGB): A PCL XYZRGB point cloud
+
+        Returns:
+            PointCloud2: A ROS point cloud
+    """
+    ros_msg = PointCloud2()
+
+    ros_msg.header.stamp = rospy.Time.now()
+    ros_msg.header.frame_id = "world"
+
+    ros_msg.height = 1
+    ros_msg.width = pcl_array.size
+
+    ros_msg.fields.append(PointField(
+                            name="x",
+                            offset=0,
+                            datatype=PointField.FLOAT32, count=1))
+    ros_msg.fields.append(PointField(
+                            name="y",
+                            offset=4,
+                            datatype=PointField.FLOAT32, count=1))
+    ros_msg.fields.append(PointField(
+                            name="z",
+                            offset=8,
+                            datatype=PointField.FLOAT32, count=1))
+    ros_msg.fields.append(PointField(
+                            name="rgb",
+                            offset=16,
+                            datatype=PointField.FLOAT32, count=1))
+
+    ros_msg.is_bigendian = False
+    ros_msg.point_step = 32
+    ros_msg.row_step = ros_msg.point_step * ros_msg.width * ros_msg.height
+    ros_msg.is_dense = False
+    buffer = []
+
+    for data in pcl_array:
+        s = struct.pack('>f', data[3])
+        i = struct.unpack('>l', s)[0]
+        pack = ctypes.c_uint32(i).value
+
+        r = (pack & 0x00FF0000) >> 16
+        g = (pack & 0x0000FF00) >> 8
+        b = (pack & 0x000000FF)
+
+        buffer.append(struct.pack('ffffBBBBIII', data[0], data[1], data[2], 1.0, b, g, r, 0, 0, 0, 0))
+
+    ros_msg.data = "".join(buffer)
+
+    return ros_msg
+
+def callback(data):
+	p = ros_to_pcl(data)
+	seg = p.make_segmenter()
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    indices, model = seg.segment()
+
+    cloud = p.extract(indices, negative=False)
+    ros_cloud = pcl_to_ros(cloud)
+    pub.publish(ros_cloud)
+
+rospy.init_node('listener', anonymous=True)
+rospy.Subscriber("/camera/depth_registered/points", PointCloud2, callback)
+pub = rospy.Publisher("filterd_cloud", PointCloud2, queue_size = 10)
+rospy.spin()
