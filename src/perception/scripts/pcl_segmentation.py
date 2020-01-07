057b315a755b158489b4b3d55cffd8a3eb92b0a3
saa1605 <saaket.agashe@gmail.com>
2020-01-07 13:17:52 +0530

    added seg code

--- src/perception/scripts/pcl_segmentation.py
+++ src/perception/scripts/pcl_segmentation.py
@@ -2,9 +2,13 @@
 
 import rospy
 import pcl
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, PointField
 import sensor_msgs.point_cloud2 as pc2
 import ros_numpy
+from std_msgs.msg import Header
+from random import randint
+import ctypes
+import struct
 
 
 def ros_to_pcl(ros_cloud):
@@ -82,17 +86,99 @@ def pcl_to_ros(pcl_array):
     return ros_msg
 
 def callback(data):
-	p = ros_to_pcl(data)
-	seg = p.make_segmenter()
-    seg.set_model_type(pcl.SACMODEL_PLANE)
-    seg.set_method_type(pcl.SAC_RANSAC)
-    indices, model = seg.segment()
 
-    cloud = p.extract(indices, negative=False)
-    ros_cloud = pcl_to_ros(cloud)
+    cloud = ros_to_pcl(data)
+
+    vox = cloud.make_voxel_grid_filter()
+
+    # Choose a voxel (also known as leaf) size
+    # Note: this (1) is a poor choice of leaf size  
+    # Experiment and find the appropriate size!
+    LEAF_SIZE = 0.05  
+
+    # Set the voxel (or leaf) size  
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+    # Call the filter function to obtain the resultant downsampled point cloud
+    cloud_filtered = vox.filter()
+    filename = 'voxel_downsampled.pcd'
+    pcl.save(cloud_filtered, filename)
+
+
+    ### PassThrough filter
+    # Create a PassThrough filter object.
+    passthrough = cloud_filtered.make_passthrough_filter()
+
+    # Assign axis and range to the passthrough filter object.
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+    axis_min = 0.758
+    axis_max = 1.1
+    passthrough.set_filter_limits(axis_min, axis_max)
+
+    # Finally use the filter function to obtain the resultant point cloud.
+    cloud_filtered = passthrough.filter()
+    # filename = 'pass_through_filtered.pcd'
+    # pcl.save(cloud_filtered, filename)
+
+    # print(cloud_filtered)
+    # ### RANSAC plane segmentation
+    # # Create the segmentation object
+    # seg = cloud_filtered.make_segmenter()
+
+    # # Set the model you wish to fit
+    # seg.set_model_type(pcl.SACMODEL_PLANE)
+    # seg.set_method_type(pcl.SAC_RANSAC)
+
+    # # Max distance for a point to be considered fitting the model
+    # # Experiment with different values for max_distance
+    # # for segmenting the table
+    # max_distance = 0.01
+    # seg.set_distance_threshold(max_distance)
+
+    # # Call the segment function to obtain set of inlier indices and model coefficients
+    # inliers, coefficients = seg.segment()
+
+
+    # ### Extract inliers
+    # extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+    # filename = 'extracted_inliers.pcd'
+    # pcl.save(extracted_inliers, filename)
+    # # This will give us just the table
+
+
+    # ### Extract outliers
+    # extracted_outliers = cloud_filtered.extract(inliers, negative=True)
+    # filename = 'extracted_outliers.pcd'
+    # pcl.save(extracted_outliers, filename)
+    # # This will give us just the objects
+
+
+    # ### Statistical Outlier Removal Filter
+    # # Not necessary for our test object, but will be used later on...
+
+    # # Much like the previous filters, we start by creating a filter object:
+    # outlier_filter = cloud_filtered.make_statistical_outlier_filter()
+
+    # # Set the number of neighboring points to analyze for any given point
+    # outlier_filter.set_mean_k(50)
+
+    # # Set threshold scale factor
+    # x = 1.0
+
+    # # Any point with a mean distance larger than global (mean distance+x*std_dev) will be considered outlier
+    # outlier_filter.set_std_dev_mul_thresh(x)
+
+    # # Finally call the filter function for magic
+    # cloud_filtered = outlier_filter.filter()
+
+    # print(ros_cloud)
+    ros_cloud = pcl_to_ros(cloud_filtered)
+   
     pub.publish(ros_cloud)
 
 rospy.init_node('listener', anonymous=True)
 rospy.Subscriber("/camera/depth_registered/points", PointCloud2, callback)
 pub = rospy.Publisher("filterd_cloud", PointCloud2, queue_size = 10)
 rospy.spin()
+
