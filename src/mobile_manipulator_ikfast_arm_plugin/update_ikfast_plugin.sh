a6b271558a5e9517572d699d35268c2ba07a54ab
shambhavi19 <kutheshambhavi1@gmail.com>
2020-01-14 11:45:51 +0530

    new setup with end effector links

--- src/mobile_manipulator_ikfast_arm_plugin/update_ikfast_plugin.sh
+++
@@ -1,1 +0,0 @@
-rosrun moveit_kinematics create_ikfast_moveit_plugin.py mobile_manipulator arm mobile_manipulator_ikfast_arm_plugin /home/avitra/manipulator_ws/src/mobile_manipulator_ikfast_arm_plugin/src/mobile_manipulator_arm_ikfast_solver.cpp
