53aa4c97e0cdd8fcd0ce31629d1b1aece7f6f2a9
shambhavi19 <kutheshambhavi1@gmail.com>
2020-03-02 14:35:47 +0530

    changed go_to_point for pose_sub.py

--- src/move_group/scripts/go_to_point.py
+++ src/move_group/scripts/go_to_point.py
@@ -2,4 +2,5 @@
 
 import os
 
-os.system('rosrun move_group grasp_pose_sub.py && rosrun move_group cartesian.py && rosrun move_group gripper.py close')
+# os.system('rosrun move_group grasp_pose_sub.py && rosrun move_group cartesian.py && rosrun move_group gripper.py close')
+os.system('rosrun move_group pose_sub.py && rosrun move_group cartesian.py && rosrun move_group gripper.py close')
