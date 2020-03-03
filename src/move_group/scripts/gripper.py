0ade49be61336bec8994b011fbab23bf1c10113b
shambhavi19 <kutheshambhavi1@gmail.com>
2020-03-03 12:12:28 +0530

    urdf reverted back. Updated gripper.py

--- src/move_group/scripts/gripper.py
+++ src/move_group/scripts/gripper.py
@@ -15,36 +15,40 @@ import numpy as np
 
 max_load = 0.125 #0.0624
 current_load = 0.0
-angle = -0.015#-2.0
+open_angle = -0.015 #-2.0
+close_angle = -1.0
+angle = 0
 
-def State_cb(data):
-    current_load = data.load
+# def State_cb(data):
+#     current_load = data.load
 
-rospy.Subscriber("/gripper_controller/state", JointState, State_cb)
+# rospy.Subscriber("/gripper_controller/state", JointState, State_cb)
 pub = rospy.Publisher("/gripper_controller/command", Float64, queue_size=10)
 rospy.init_node('gripper', anonymous=True)
 
 
 def gripper_close():
 	global angle
-	gripper_open()
+	global close_angle
+	# gripper_open()
 	# while((current_load < max_load) & (angle >= -0.84)):
-	while(angle >= -0.84):
+	while(angle >= close_angle):
 		pub.publish(angle)
 		angle -= 0.009
 
 def gripper_open():
-    pub.publish(-0.015)
+	global open_angle
+    	pub.publish(open_angle)
 
 
 if __name__ == '__main__':
 	if (len(sys.argv) == 2):
-	    if (sys.argv[1] == "open"):
-	        gripper_open()
-	    elif(sys.argv[1] == "close"):
-	        gripper_close()
-	    else:
-	        "Supported Arguments : open or close"
+		if (sys.argv[1] == "open"):
+	        	gripper_open()
+		elif(sys.argv[1] == "close"):
+	        	gripper_close()
+	    	else: 
+			"Supported Arguments : open or close"
 	else: 
 		gripper_open()
   
